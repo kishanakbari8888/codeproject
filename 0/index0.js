5f6ed6283e83d793f35d8dcea4d6be691857af66
kishan <kishanakbari@gmail.com>
2022-10-18 21:17:24 +0530

    here i used git

---
+++ 0/index0.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url=='/about')
+    {
+        res.end("<h1> kishan <h1>");
+    }
+    else if(req.url=='/'){
+        res.end('hello kishan akbari');
+    }
+    else
+    {
+        res.end("error");
+    }
+    
+});
+
+
+
+server.listen(2000);
