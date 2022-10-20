0f0887f5ae28784a0aaeedeff660513d388cb558
kishan <aks565516sadmar@gmail.com>
2022-10-20 15:57:03 +0530

    demo 6 commit

--- 0/index0.js
+++ 0/index0.js
@@ -4,6 +4,7 @@ const server = http.createServer((req,res)=>{
     if(req.url=='/about')
     {
         res.end("<h1> kishan <h1>");
+        
     }
     else if(req.url=='/'){
         res.end('hello kishan akbari');
